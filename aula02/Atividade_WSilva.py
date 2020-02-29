30a7af11fc98b087ecca811a94ec492fa03b64ba
williamars <williamars@al.insper.edu.br>
2020-02-29 13:00:30 -0300

    Testando reduzir as janelas

--- aula02/Atividade_WSilva.py
+++ aula02/Atividade_WSilva.py
@@ -33,6 +33,29 @@ while(True):
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+    
+    #
+    blur = cv2.GaussianBlur(gray,(5,5),0) 
+    bordas = auto_canny(blur)
+
+    circles = []
+
+    # Obtains a version of the edges image where we can draw in color
+    bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
+
+    # HoughCircles - detects circles using the Hough Method. For an explanation of
+    circles = None
+    circles=cv2.HoughCircles(bordas,cv2.HOUGH_GRADIENT,2,40,param1=50,param2=100,minRadius=5,maxRadius=60)
+
+    if circles is not None:        
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            print(i)
+            # draw the outer circle
+            # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
+            cv2.circle(bordas_color,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(bordas_color,(i[0],i[1]),2,(0,0,255),3)
    
     # Rosa
     cap_pink1, cap_pink2 = aux.ranges(pink)
@@ -56,11 +79,13 @@ while(True):
     # Fazendo a seleção de ambos
     selecao = selecao_pink + selecao_blue
     
-    # CONTORNO ROSA
+    cv2.imshow("selecao", selecao)
+    
+    # CONTORNO do ROSA
     segmentado_pink = cv2.morphologyEx(mask_pink,cv2.MORPH_CLOSE,np.ones((4, 4)))
     img_out_pink, contornos_pink, arvore_rosa = cv2.findContours(segmentado_pink.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contornos_img_pink = frame.copy()
-    cv2.drawContours(contornos_img_pink, contornos_pink, -1, [0, 0, 255], 3);
+    contornos = frame.copy()
+    cv2.drawContours(contornos, contornos_pink, -1, [0, 0, 255], 3);
     
     maior_pink = None
     maior_area_pink = 0
@@ -69,14 +94,11 @@ while(True):
         if area_pink > maior_area_pink:
             maior_area_pink = area_pink
             maior_pink = c
-            
-    cv2.drawContours(contornos_img_pink, [maior_pink], -1, [0, 255, 255], 5);
     
-    # CONTORNO AZUL
+    # CONTORNO do AZUL
     segmentado_blue = cv2.morphologyEx(mask_blue,cv2.MORPH_CLOSE,np.ones((4, 4)))
     img_out_blue, contornos_blue, arvore_rosa = cv2.findContours(segmentado_blue.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contornos_img_blue = frame.copy()
-    cv2.drawContours(contornos_img_blue, contornos_blue, -1, [0, 0, 255], 3);
+    cv2.drawContours(contornos, contornos_blue, -1, [0, 0, 255], 3);
     
     maior_blue = None
     maior_area_blue = 0
@@ -85,43 +107,12 @@ while(True):
         if area_blue > maior_area_blue:
             maior_area_blue = area_blue
             maior_blue = c
-            
-            
-    blur = cv2.GaussianBlur(gray,(5,5),0) 
-    bordas = auto_canny(blur)
-
-
-    circles = []
-
-
-    # Obtains a version of the edges image where we can draw in color
-    bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
-
-    # HoughCircles - detects circles using the Hough Method. For an explanation of
-    circles = None
-    circles=cv2.HoughCircles(bordas,cv2.HOUGH_GRADIENT,2,40,param1=50,param2=100,minRadius=5,maxRadius=60)
-
-    if circles is not None:        
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            print(i)
-            # draw the outer circle
-            # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
-            cv2.circle(bordas_color,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv2.circle(bordas_color,(i[0],i[1]),2,(0,0,255),3)        
-    
-    cv2.imshow("contornos_img_blue", contornos_img_blue) 
-    cv2.imshow("contornos_img_pink", contornos_img_pink)
-    cv2.imshow("Circles", bordas_color)
-    
-    # Display the resulting frame
-    # cv2.imshow('gray', gray)
-    # cv2.imshow("mask", mask)
-    cv2.imshow("selecao", selecao)
-    
+           
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(bordas_color,'Press q to quit',(0,50), font, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(contornos,'q to quit',(0,50), font, 1,(255,255,255),2,cv2.LINE_AA)
+    
+    cv2.imshow("contornos", contornos)
+    cv2.imshow("circles", bordas_color)
 
     # Fechar as abas se apertar 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ aula02/untitled1.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import auxiliar as aux
+
+# Parameters to use when opening the webcam.
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+lower = 0
+upper = 1
+
+# Cores a serem identificadas
+pink = '#ff004d'
+blue = '#0173fe'
+
+# Returns an image containing the borders of the image
+# sigma is how far from the median we are setting the thresholds
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # A gaussian blur to get rid of the noise in the image
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    # Detect the edges present in the image
+    bordas = auto_canny(blur)
+
+    circles = []
+
+    # Obtains a version of the edges image where we can draw in color
+    bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
+
+    circles = None
+    circles=cv2.HoughCircles(bordas,cv2.HOUGH_GRADIENT,2,40,param1=50,param2=100,minRadius=5,maxRadius=60)
+
+    if circles is not None:        
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            print(i)
+            # draw the outer circle
+            # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
+            cv2.circle(bordas_color,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(bordas_color,(i[0],i[1]),2,(0,0,255),3)
+
+    # Rosa
+    cap_pink1, cap_pink2 = aux.ranges(pink)
+    mask_pink = cv2.inRange(bordas_color, cap_pink1, cap_pink2)
+    
+    # Blue 
+    cap_blue1, cap_blue2 = aux.ranges(blue)
+    mask_blue = cv2.inRange(bordas_color, cap_blue1, cap_blue2)
+    
+    # Construindo a máscara de ambos
+    mask = mask_blue + mask_pink
+    
+    cv2.imshow('mask',mask)
+    
+    # Display the resulting frame
+    cv2.imshow('Detector de circulos',bordas_color)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#  When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
